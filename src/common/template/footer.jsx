8c4afb9c11fdecc3c9488f97a6a1934e6a82d0ec
fernando.groeler <fernando.groeler@gmail.com>
2018-02-27 08:35:19 -0300

    Initial commit

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2018
+            <a href='https://github.com/FernandoGroeler' target='_blank'> Fernando Groeler</a>
+        </strong>
+    </footer>
+)
