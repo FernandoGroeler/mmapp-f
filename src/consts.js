398dd5a3014a786959222e3d9879a00049d4a83e
fernando.groeler <fernando.groeler@gmail.com>
2018-02-28 16:00:40 -0300

    Ajustando para fazer o deploy.

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,8 @@
 export default {
+    /*
     API_URL: 'http://localhost:3003/api',
     OAPI_URL: 'http://localhost:3003/oapi'
+    */
+    API_URL: 'https://mmapp-b.herokuapp.com/api',
+    OAPI_URL: 'https://mmapp-b.herokuapp.com/oapi'   
 }
